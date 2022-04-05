8fca3c7eab552fa238d3bf2cba2a964c3597537d
JohnDavidCabal <Johndavidcabal000@gmail.com>
2022-04-05 18:58:17 +0800

    modifeid

--- main.java
+++ main.java
@@ -5,11 +5,6 @@ import service.EncryptMessage;
 public class main {
     public static void main(String[] args) {
        
-        // EncryptMessage message = new EncryptMessage(5, "Hello");
-        // message.encryptMessages();
-
-        // DecryptMessage secondMessage = new DecryptMessage(4, "NSLR HEZMH");
-        // secondMessage.decryptMessages();
 
         CaesarCipherImpl imp = new CaesarCipherImpl();
         imp.startEcnryptionAndDecryption();
